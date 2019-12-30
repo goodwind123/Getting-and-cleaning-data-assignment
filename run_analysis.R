c18027e8c783bdc07bb368c1316ba8cc78f740c1
goodwind123 <50448239+goodwind123@users.noreply.github.com>
2019-12-30 17:12:18 +0800

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,37 @@
+#reading data
+x_train<-read.table("./UCI HAR Dataset/train/X_train.txt")
+y_train<-read.table("./UCI HAR Dataset/train/y_train.txt")
+subj_train<-read.table("./UCI HAR Dataset/train/subject_train.txt")
+x_test<-read.table("./UCI HAR Dataset/test/X_test.txt")
+y_test<-read.table("./UCI HAR Dataset/test/y_test.txt")
+subj_test<-read.table("./UCI HAR Dataset/test/subject_test.txt")
+
+#column names
+activitynames<-read.table("./UCI HAR Dataset/activity_labels.txt")
+features<-read.table("./UCI HAR Dataset/features.txt")
+
+#merge x and y
+x<-rbind(x_test,x_train)
+y<-rbind(y_test,y_train)
+subj<-rbind(subj_test,subj_train)
+
+#subset mean and std from x
+features<-features[grep("mean|std",features[,2]),]
+x<-x[,features[,1]]
+colnames(x)<-features[,2]
+colnames(subj)<-"subject"
+
+#labels for y
+colnames(y)<-"activity"
+y$activitylabel<-factor(y$activity,labels=as.character(activitynames[,2]))
+
+#merge everything
+all<-cbind(subj,y,x)
+all_summary<-all%>%
+  group_by(activitylabel,subject) %>%
+  summarize_all(funs(mean))
+#export table
+write.table(all_summary,file="./UCI HAR Dataset/tidydata.txt",col.names=TRUE,row.names=FALSE)
+
+
+  
